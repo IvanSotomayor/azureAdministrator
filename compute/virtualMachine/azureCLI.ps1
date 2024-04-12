c4235e78a9a4da79dd0edc63d648db3f74cb5d22
Ortega <ivan.ortega@softwareone.com>
2024-04-12 15:37:45 -0600

    add vm creation scripts

---
+++ compute/virtualMachine/azureCLI.ps1
@@ -0,0 +1,15 @@
+New-AzResourceGroup -Name 'myResourceGroup' -Location 'EastUS'
+
+New-AzVm `
+    -ResourceGroupName 'myResourceGroup' `
+    -Name 'myVM' `
+    -Location 'East US' `
+    -Image 'MicrosoftWindowsServer:WindowsServer:2022-datacenter-azure-edition:latest' `
+    -VirtualNetworkName 'myVnet' `
+    -SubnetName 'mySubnet' `
+    -SecurityGroupName 'myNetworkSecurityGroup' `
+    -PublicIpAddressName 'myPublicIpAddress' `
+    -OpenPorts 80,3389
+
+
+    Invoke-AzVMRunCommand -ResourceGroupName 'myResourceGroup' -VMName 'myVM' -CommandId 'RunPowerShellScript' -ScriptString 'Install-WindowsFeature -Name Web-Server -IncludeManagementTools'
